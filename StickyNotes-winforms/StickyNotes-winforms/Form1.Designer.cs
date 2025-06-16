a4abb305f45b7b93a30c52627f6ba53965790833
torkel <torkel.ivinger@elev.ga.lbs.se>
2025-06-16 23:50:43 +0200

    Color picker & Fonts
    made color picker & font picker, also tried to use anchors

--- StickyNotes-winforms/StickyNotes-winforms/Form1.Designer.cs
+++ StickyNotes-winforms/StickyNotes-winforms/Form1.Designer.cs
@@ -31,6 +31,9 @@
             richTextBox1 = new RichTextBox();
             topmostWindowCheck = new CheckBox();
             newwinbutton = new Button();
+            bgcolorBtn = new Button();
+            fontBtn = new Button();
+            fontDialog1 = new FontDialog();
             SuspendLayout();
             // 
             // richTextBox1
@@ -38,7 +41,8 @@
             richTextBox1.AcceptsTab = true;
             richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             richTextBox1.BackColor = Color.White;
-            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Cursor = Cursors.IBeam;
             richTextBox1.EnableAutoDragDrop = true;
             richTextBox1.Font = new Font("Segoe Print", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             richTextBox1.Location = new Point(3, 25);
@@ -51,31 +55,76 @@
             // topmostWindowCheck
             // 
             topmostWindowCheck.AutoSize = true;
-            topmostWindowCheck.Dock = DockStyle.Top;
+            topmostWindowCheck.BackColor = Color.Silver;
+            topmostWindowCheck.Cursor = Cursors.Hand;
+            topmostWindowCheck.FlatStyle = FlatStyle.Popup;
             topmostWindowCheck.Location = new Point(0, 0);
             topmostWindowCheck.Name = "topmostWindowCheck";
-            topmostWindowCheck.Size = new Size(400, 19);
+            topmostWindowCheck.Size = new Size(109, 19);
             topmostWindowCheck.TabIndex = 3;
             topmostWindowCheck.Text = "Window On Top";
-            topmostWindowCheck.UseVisualStyleBackColor = true;
+            topmostWindowCheck.UseVisualStyleBackColor = false;
             topmostWindowCheck.CheckedChanged += checkBox1_CheckedChanged;
             // 
             // newwinbutton
             // 
             newwinbutton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            newwinbutton.BackColor = Color.Silver;
+            newwinbutton.Cursor = Cursors.Hand;
+            newwinbutton.FlatStyle = FlatStyle.Popup;
+            newwinbutton.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold);
             newwinbutton.Location = new Point(325, 0);
             newwinbutton.Name = "newwinbutton";
-            newwinbutton.Size = new Size(75, 23);
+            newwinbutton.Size = new Size(75, 19);
             newwinbutton.TabIndex = 4;
             newwinbutton.Text = "New Note";
-            newwinbutton.UseVisualStyleBackColor = true;
+            newwinbutton.UseVisualStyleBackColor = false;
             newwinbutton.Click += newwinbutton_Click;
+            // 
+            // bgcolorBtn
+            // 
+            bgcolorBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            bgcolorBtn.BackColor = Color.Silver;
+            bgcolorBtn.Cursor = Cursors.Hand;
+            bgcolorBtn.FlatStyle = FlatStyle.Popup;
+            bgcolorBtn.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold);
+            bgcolorBtn.Location = new Point(200, 0);
+            bgcolorBtn.Name = "bgcolorBtn";
+            bgcolorBtn.Size = new Size(119, 19);
+            bgcolorBtn.TabIndex = 5;
+            bgcolorBtn.Text = "Background Color";
+            bgcolorBtn.UseVisualStyleBackColor = false;
+            bgcolorBtn.Click += bgcolorBtn_Click;
+            // 
+            // fontBtn
+            // 
+            fontBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            fontBtn.BackColor = Color.Silver;
+            fontBtn.Cursor = Cursors.Hand;
+            fontBtn.FlatStyle = FlatStyle.Popup;
+            fontBtn.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold);
+            fontBtn.Location = new Point(140, 0);
+            fontBtn.Name = "fontBtn";
+            fontBtn.Size = new Size(54, 19);
+            fontBtn.TabIndex = 6;
+            fontBtn.Text = "Fonts";
+            fontBtn.UseVisualStyleBackColor = false;
+            fontBtn.Click += fontBtn_Click;
+            // 
+            // fontDialog1
+            // 
+            fontDialog1.ShowApply = true;
+            fontDialog1.ShowColor = true;
+            fontDialog1.Apply += fontDialog1_Apply_1;
             // 
             // StickyNote
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(400, 450);
+            Controls.Add(fontBtn);
+            Controls.Add(bgcolorBtn);
             Controls.Add(newwinbutton);
             Controls.Add(topmostWindowCheck);
             Controls.Add(richTextBox1);
@@ -92,5 +141,8 @@
         private RichTextBox richTextBox1;
         private CheckBox topmostWindowCheck;
         private Button newwinbutton;
+        private Button bgcolorBtn;
+        private Button fontBtn;
+        private FontDialog fontDialog1;
     }
 }
--- StickyNotes-winforms/StickyNotes-winforms/Form1.cs
+++ StickyNotes-winforms/StickyNotes-winforms/Form1.cs
@@ -1,3 +1,6 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
 namespace StickyNotes_winforms
 {
     public partial class StickyNote : Form
@@ -45,6 +48,54 @@ namespace StickyNotes_winforms
             {
                 Application.Exit();
             }
+        }
+
+        private void bgcolorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog bgcolorDialog = new ColorDialog();
+            bgcolorDialog.AllowFullOpen = true;
+            bgcolorDialog.AnyColor = true;
+            bgcolorDialog.ShowHelp = false;
+            bgcolorDialog.Color = this.BackColor;
+
+            if (bgcolorDialog.ShowDialog() == DialogResult.OK)
+                this.BackColor = bgcolorDialog.Color;
+            richTextBox1.BackColor = bgcolorDialog.Color;
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontBtn_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;
+
+            fontDialog1.Font = richTextBox1.Font;
+            fontDialog1.Color = richTextBox1.ForeColor;
+
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                richTextBox1.Font = fontDialog1.Font;
+                richTextBox1.ForeColor = fontDialog1.Color;
+            }
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
